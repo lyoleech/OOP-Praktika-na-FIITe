2ee9945bbbfa4dda2cc0be60a24d1ec6b55e6235
lyoleech <59200942+lyoleech@users.noreply.github.com>
2019-12-25 19:49:13 +0300

    Add files via upload

---
+++ zoo/main.cpp
@@ -0,0 +1,22 @@
+//
+//  main.cpp
+//  zoo
+//
+//  Created by Olga Chernova on 11/07/2019.
+//  Copyright © 2019 Olga Chernova. All rights reserved.
+//
+
+#include <iostream>
+#include <vector>
+#include <ctime>
+#include <memory>
+#include <string>
+
+#include "zoopark.hpp"
+
+int main()
+{
+    Zoopark fiitess;
+    fiitess.Action();
+    return 0;
+}
